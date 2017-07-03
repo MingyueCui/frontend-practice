5f9988a7f36684fe29732eac4066048405701066
崔明月 <MingyueCui@cuimingyuedeMacBook-Pro.local>
2017-07-03 17:20:14 +0800

    localstorage

--- cuimingyue/todo.js
+++ cuimingyue/todo.js
@@ -1,10 +1,12 @@
 $(function () {
-  alert(localStorage.length);
   var $boxs = $('.boxs');
-  for(i=0;i<localStorage.length;i++){
-    var html='<div class="box"><i class="notdone fa fa-circle-thin " aria-hidden="true" ></i><img src="done.png" class="done-image">  <textarea  class="content" >'+localStorage.key(i)+'" </textarea><i class="trash fa fa-trash " aria-hidden="true" ></i></div>';
-   $boxs.html($boxs.html()+html);
 
+  for(i=0;i<localStorage.length;i++){
+    //alert(localStorage[localStorage.key(i)]);
+    if(localStorage[localStorage.key(i)]==1){
+    var html='<div class="box"><i class="notdone fa fa-circle-thin " aria-hidden="true" ></i><img src="done.png" class="done-image">  <textarea  class="content" >'+localStorage.key(i)+'" </textarea><i class="trash fa fa-trash " aria-hidden="true" ></i></div>';
+    $boxs.html($boxs.html()+html);
+ }
 }
 $('#myinput').keydown(function(e){
   var $input = $('#myinput');
@@ -16,26 +18,25 @@ $('#myinput').keydown(function(e){
   }
 });
 $(document).on('click', '.trash', function(e){
+  var del_content=$(e.target).prev().val();
+  localStorage.removeItem(del_content);
+  $(e.target).parent().remove();
   $(e.target).parent().hide();
 })
 $(document).on('click', '.notdone', function(e){
   var className=$(this).attr('class');
-  if(className.indexOf("fa-circle-thin")!=-1){
+  var keyname=$(e.target).next().next().val();
+  if(className.indexOf("fa-circle-thin")!=-1){//未完成向已完成转化
+    localStorage.removeItem(keyname);
     $(e.target).removeClass("fa-circle-thin");
     $(e.target).addClass("fa-check-circle-o");
-    $(e.target).next().css('visibility','visible');
+    $(e.target).next().css('visibility','visible');//盖章
   }
-  else{
+  else{//已经完成被标记成未完成
     $(e.target).removeClass("fa-check-circle-o");
     $(e.target).addClass("fa-circle-thin");
     $(e.target).next().css('visibility','hidden')
   }
-  $(document).on('click', '.trash', function(e){
-    var del_content=$(e.target).prev().val();
-    localStorage.remove(del_content);
-    $(e.target).parent().remove();
-  })
-
 })
 
 });
