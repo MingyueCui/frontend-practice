f137554a0506171dd22f4f58296b82bccc2bf512
崔明月 <MingyueCui@cuimingyuedeMacBook-Pro.local>
2017-07-03 16:06:20 +0800

    update

--- cuimingyue/todo.js
+++ cuimingyue/todo.js
@@ -5,25 +5,30 @@ $('#myinput').keydown(function(e){
   var $input = $('#myinput');
   if(e.keyCode==13){//回车时间绑定
     var value=$input.val();
-    var text='<div class="box"><i class="notdone fa fa-circle-thin " aria-hidden="true" ></i><p class="content">'+value+'</p><i class="trash fa fa-trash " aria-hidden="true" ></i></div>';
+    var text='<div class="box"><i class="notdone fa fa-circle-thin " aria-hidden="true" ></i><img src="done.png" class="done-image">  <textarea  class="content" >'+value+'" </textarea><i class="trash fa fa-trash " aria-hidden="true" ></i></div>';
     $boxs.html($boxs.html()+text);//增加新的box
   }
 });
 $(document).on('click', '.trash', function(e){
-  $(e.target).parent().remove()
+  $(e.target).parent().hide()
 })
 $(document).on('click', '.notdone', function(e){
   var className=$(this).attr('class');
   if(className.indexOf("fa-circle-thin")!=-1){
     $(e.target).removeClass("fa-circle-thin");
     $(e.target).addClass("fa-check-circle-o");
+    $(e.target).next().css('visibility','visible')
     $(e.target).parent().css('text-decoration', 'line-through');
   }
   else{
     $(e.target).removeClass("fa-check-circle-o");
     $(e.target).addClass("fa-circle-thin");
+      $(e.target).next().css('visibility','hidden')
     $(e.target).parent().css('text-decoration', 'none');
   }
+  $(document).on('click', '.trash', function(e){
+    $(e.target).parent().remove();
+  })
 
 })
 
